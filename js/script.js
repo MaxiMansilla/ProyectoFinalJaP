259d59ae7bc43112d879b5b857a3c9c06275fe18
Maximiliano <maximiliano.mansilla@estudiante.ceibal.edu.uy>
2018-07-01 19:56:23 -0300

    Ultimo cambiaso

--- js/script.js
+++ js/script.js
@@ -8,27 +8,28 @@ $(document).ready(function(){
 			scrollTop: $(this.hash).offset().top
 		}, )
 	});
-
-	("#form").submit(function(event){
-		event.preventDefault();
-		$.ajax({
-	    url: "https://formspree.io/maximiliano.mansilla@estudiante.ceibal.edu.uy", 
-	    method: "POST",
-	    data:{
-	    	nombre: $("#nombre").val(),
-	    	email: $("#email").val(),
-	    	asunto: $("#asunto").val(),
-	    	mensaje: $("#mensaje").val(),
-		},
-	    dataType: "json"
-		}).done(function(){
-			$("#nombre").val(""),
-	    	$("#email").val(""),
-	    	$("#asunto").val(""),
-	    	$("#mensaje").val(""),
-			alert('Mensaje enviado con éxito, en breve me estaré comunicando!');
-		}).fail(function(){
-			alert('Error de mensaje, vuelve a intentarlo mas tarde!');
-		})
+	$(function($){
+		("form").submit(function(event){
+			event.preventDefault();
+			$.ajax({
+		    url: "https://formspree.io/maximiliano.mansilla@estudiante.ceibal.edu.uy", 
+		    method: "POST",
+		    data:{
+		    	nombre: $("#nombre").val(),
+		    	email: $("#email").val(),
+		    	asunto: $("#asunto").val(),
+		    	mensaje: $("#mensaje").val(),
+			},
+		    dataType: "json"
+			}).done(function(){
+				$("#nombre").val(""),
+		    	$("#email").val(""),
+		    	$("#asunto").val(""),
+		    	$("#mensaje").val(""),
+				alert('Mensaje enviado con éxito, en breve me estaré comunicando!');
+			}).fail(function(){
+				alert('Error de mensaje, vuelve a intentarlo mas tarde!');
+			});
+		});
 	});
 });
